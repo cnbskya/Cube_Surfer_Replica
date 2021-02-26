2da553963f1aab785f881d87fd2e903c4e1bf559
Halim Can Başkaya <cnbskya@gmail.com>
2021-02-27 00:35:06 +0300

    Fist Folders Create and Added

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+	public bool isGameOn;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	private void Start()
+	{
+		OnGameStart();
+	}
+
+	public void OnGameStart()
+	{
+		isGameOn = true;
+	}
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	public static UIManager instance;
+
+	// UI Ekranında gösterilecek olan şeyler bu script dosyasında tutulacak
+
+	private void Awake()
+	{
+		instance = this;
+	}
+}
