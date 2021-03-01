e3fc383127e581ae84f3a91044553678ed5a2868
Halim Can Başkaya <cnbskya@gmail.com>
2021-03-01 21:32:57 +0300

    Added Bonus Process

--- Assets/Scripts/MovementCube.cs
+++ Assets/Scripts/MovementCube.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MovementCube : MonoBehaviour
 {
-    Rigidbody rb;
-    public float forwardSpeed;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	public Rigidbody rb;
+	public float forwardSpeed;
+	public List<GameObject> stackCubes;
+	[Space(50)]
+	public GameObject childPrefab;
 
 	private void FixedUpdate()
 	{
-        rb.velocity = Vector3.forward * forwardSpeed * Time.deltaTime;
+		rb.velocity = Vector3.forward * forwardSpeed * Time.deltaTime;
 	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			Debug.Log("Çalıştı");
+			stackCubes.Remove(gameObject);
+			transform.SetParent(null);
+		}
+		if (collision.gameObject.CompareTag("BonusCube"))
+		{
+			AddBonusCube(collision);
+
+		}
+	}
+
+
+	public void AddBonusCube(Collision collision)
+	{
+		Destroy(collision.gameObject); // ÇARPTIĞIMIZ BONUS SİLİNDİ
+		var go = Instantiate(childPrefab); // YENİ BİR GAMEOBJECT ÜRETİLDİ
+		go.transform.position = stackCubes[stackCubes.Count -1].transform.position; // DİZİNİN O ANKİ EN ALTTAKİ ELAMANIN YERİNE YENİ OLUŞAN GO GETİRİLDİ
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x, transform.position.y + go.transform.localScale.y, transform.position.z); // ONCEKİ DİZİ ELEMANLARI TOPLU ŞEKİLDE YUKARI ÇIKARILDI.
+		go.transform.SetParent(gameObject.transform); // YENİ EKLENEN OBJENİN HAREKETİ SAĞLAMASI İÇİN CHİLD YAPILDI
+		stackCubes.Add(go); // BİR SONRAKİ YER AYARLAMA İŞLEMİNİN DÜZGÜN OLMASI İÇİN DİZİYE EKLENDİ.
+		
+	}
+
+
 }
